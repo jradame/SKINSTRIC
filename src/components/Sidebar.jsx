7bd52c29afe91d5a10b4dc487429c1fd9a80b2c7
justin <jradame@gmail.com>
2025-09-06 09:07:15 -0500

    SKINTRIC

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,23 +5,26 @@ const Sidebar = () => {
   return (
     <>
       {/* Left Sidebar */}
-      <div className="fixed left-6 top-1/2 transform -translate-y-1/2 z-40">
-        <div className="flex items-center space-x-2 cursor-pointer hover:opacity-70 transition-opacity">
+      <div className="fixed left-8 top-1/2 transform -translate-y-1/2 z-40">
+        <div className="flex items-center space-x-3 cursor-pointer hover:opacity-70 transition-opacity">
           <img 
             src="/Image/buttin-icon-shrunk.png" 
             alt="button icon" 
-            className="w-7 h-7 object-contain" 
+            className="w-10 h-10 object-contain" 
           />
-          <span className="text-[10px] text-black tracking-wider font-roobert font-extralight">DISCOVER ALL</span>
-          {/* <FiArrowLeft className="w-4 h-4" /> */}
+          <span className="text-[14px] text-black tracking-wider font-roobert font-extralight">DISCOVER ALL</span>
         </div>
       </div>
       
       {/* Right Sidebar */}
-      <div className="fixed right-6 top-1/2 transform -translate-y-1/2 z-40">
+      <div className="fixed right-8 top-1/2 transform -translate-y-1/2 z-40">
         <div className="flex items-center space-x-3 cursor-pointer hover:opacity-70 transition-opacity">
-          <span className="text-[10px] text-black tracking-wider font-roobert font-extralight">TIME TEST</span>
-          {/* <FiArrowRight className="w-4 h-4" /> */}
+          <span className="text-[14px] text-black tracking-wider font-roobert font-extralight">TIME TEST</span>
+          <img
+            src="/Image/button-icon-shrunk-right.svg"
+            alt="button icon right"
+            className="w-10 h-10 object-contain"
+          />
         </div>
       </div>
     </>
@@ -29,6 +32,8 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
+
+
 
 
 
